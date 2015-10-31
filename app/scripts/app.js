d393d0ed1a67ea50c5e8fe8e035766ecd7b40ec2
Roffel <rutger@MacBook-Pro-van-Rutger.local>
2015-10-31 08:49:20 +0100

    "working" graph
    juh

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'googlechart'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/rainfall.js
+++ app/scripts/controllers/rainfall.js
@@ -7,8 +7,9 @@
  * # RainfallCtrl
  * Controller of the weathernetApp
  */
- angular.module('weathernetApp')
- .controller('RainfallCtrl', function ($scope) {
+
+ angular.module('weathernetApp').controller("RainfallCtrl", function ($scope) {
+ 	$scope.title = "dikke pik!";
  	$scope.chartObject = {
  		"type": "LineChart",
  		"displayed": true,
@@ -20,28 +21,12 @@
  				"type": "string",
  				"p": {}
  			},
- 			{
- 				"id": "laptop-id",
- 				"label": "Laptop",
- 				"type": "number",
- 				"p": {}
- 			},
+ 			
  			{
  				"id": "desktop-id",
- 				"label": "Desktop",
+ 				"label": "Rainfall (mm)",
  				"type": "number",
  				"p": {}
- 			},
- 			{
- 				"id": "server-id",
- 				"label": "Server",
- 				"type": "number",
- 				"p": {}
- 			},
- 			{
- 				"id": "cost-id",
- 				"label": "Shipping",
- 				"type": "number"
  			}
  			],
  			"rows": [
@@ -51,19 +36,7 @@
  					"v": "January"
  				},
  				{
- 					"v": 19,
- 					"f": "42 items"
- 				},
- 				{
- 					"v": 12,
- 					"f": "Ony 12 items"
- 				},
- 				{
- 					"v": 7,
- 					"f": "7 servers"
- 				},
- 				{
- 					"v": 4
+ 					"v": 19
  				}
  				]
  			},
@@ -74,16 +47,6 @@
  				},
  				{
  					"v": 13
- 				},
- 				{
- 					"v": 1,
- 					"f": "1 unit (Out of stock this month)"
- 				},
- 				{
- 					"v": 12
- 				},
- 				{
- 					"v": 2
  				}
  				]
  			},
@@ -94,27 +57,18 @@
  				},
  				{
  					"v": 24
- 				},
- 				{
- 					"v": 5
- 				},
- 				{
- 					"v": 11
- 				},
- 				{
- 					"v": 6
  				}
  				]
  			}
  			]
  		},
  		"options": {
- 			"title": "Sales per month",
+ 			"title": "Rainfall for stn",
  			"isStacked": "true",
  			"fill": 20,
  			"displayExactValues": true,
  			"vAxis": {
- 				"title": "Sales unit",
+ 				"title": "Rainfall in mm",
  				"gridlines": {
  					"count": 10
  				}
@@ -125,9 +79,5 @@
  		},
  		"formatters": {}
  	}
-
-
-
-
 
  });
