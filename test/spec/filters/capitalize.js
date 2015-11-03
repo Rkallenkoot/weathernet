2064bfca0dcd5b6e5d6cae1baf44e33928b3e407
Roelof Kallenkoot <roelof000@gmail.com>
2015-11-03 23:37:15 +0100

    Added Filter and moved material.css style

---
+++ test/spec/filters/capitalize.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Filter: capitalize', function () {
+
+  // load the filter's module
+  beforeEach(module('weathernetApp'));
+
+  // initialize a new instance of the filter before each test
+  var capitalize;
+  beforeEach(inject(function ($filter) {
+    capitalize = $filter('capitalize');
+  }));
+
+});
